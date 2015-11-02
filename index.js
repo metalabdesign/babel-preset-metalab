1b3c05a14916c83d47e568941fceffc84c873d47
Neal Granger <neal@metalab.co>
2015-11-02 01:22:52 -0800

    Inline babel presets.
    Support for overriding presets is not currently available.

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
 module.exports = {
-  presets: ["es2015", "react"],
   plugins: [
-    // http://babeljs.io/docs/plugins/syntax-object-rest-spread
-    "syntax-object-rest-spread",
-    // http://babeljs.io/docs/plugins/transform-object-rest-spread
-    "transform-object-rest-spread",
+    // React
+    require("babel-plugin-transform-react-jsx"),
+    require("babel-plugin-transform-flow-strip-types"),
+    require("babel-plugin-syntax-flow"),
+    require("babel-plugin-syntax-jsx"),
 
-    // http://babeljs.io/docs/plugins/syntax-trailing-function-commas
-    "syntax-trailing-function-commas",
+    // ES2015
+    require("babel-plugin-transform-es2015-template-literals"),
+    require("babel-plugin-transform-es2015-literals"),
+    require("babel-plugin-transform-es2015-function-name"),
+    require("babel-plugin-transform-es2015-arrow-functions"),
+    require("babel-plugin-transform-es2015-block-scoped-functions"),
+    require("babel-plugin-transform-es2015-classes"),
+    require("babel-plugin-transform-es2015-object-super"),
+    require("babel-plugin-transform-es2015-shorthand-properties"),
+    require("babel-plugin-transform-es2015-computed-properties"),
+    require("babel-plugin-transform-es2015-for-of"),
+    require("babel-plugin-transform-es2015-sticky-regex"),
+    require("babel-plugin-transform-es2015-unicode-regex"),
+    require("babel-plugin-transform-es2015-constants"),
+    require("babel-plugin-transform-es2015-spread"),
+    require("babel-plugin-transform-es2015-parameters"),
+    require("babel-plugin-transform-es2015-destructuring"),
+    require("babel-plugin-transform-es2015-block-scoping"),
+    require("babel-plugin-transform-es2015-typeof-symbol"),
+    require("babel-plugin-transform-es2015-modules-commonjs"),
+    require("babel-plugin-transform-regenerator"),
 
-    // http://babeljs.io/docs/plugins/syntax-class-properties
-    "syntax-class-properties",
-    // http://babeljs.io/docs/plugins/transform-class-properties
-    "transform-class-properties",
+    // Object rest spread
+    require("babel-plugin-syntax-object-rest-spread"),
+    require("babel-plugin-transform-object-rest-spread"),
 
-    // http://babeljs.io/docs/plugins/syntax-function-bind
-    "syntax-function-bind",
-    // http://babeljs.io/docs/plugins/transform-function-bind
-    "transform-function-bind",
+    // Trailing function commas
+    require("babel-plugin-syntax-trailing-function-commas"),
 
-    // http://babeljs.io/docs/plugins/transform-object-assign/
-    "transform-object-assign",
+    // Class Properties
+    require("babel-plugin-syntax-class-properties"),
+    require("babel-plugin-transform-class-properties"),
 
-    // http://babeljs.io/docs/plugins/transform-node-env-inline/
-    "transform-node-env-inline",
+    // Function bind syntax
+    require("babel-plugin-syntax-function-bind"),
+    require("babel-plugin-transform-function-bind"),
 
-    "transform-es2015-modules-commonjs",
+    // Object assign polyfill
+    require("babel-plugin-transform-object-assign"),
+
+    // Inline `process.env` references
+    require("babel-plugin-transform-node-env-inline"),
+
+    // Transfrom modules to commonjs
+    require("babel-plugin-transform-es2015-modules-commonjs"),
   ],
 };
