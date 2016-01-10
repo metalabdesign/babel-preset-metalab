2b0395e92d00b7b53b246df5766d9579ade8898d
Izaak Schroeder <izaak.schroeder@gmail.com>
2016-01-10 03:19:53 -0800

    Add basic smoke test.

---
+++ test/test.js
@@ -0,0 +1,7 @@
+
+const f = (
+  a : number,
+  b : number
+) : number => a + b;
+
+console.log(f(1,2));
---
+++ test/test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+DIR=`dirname $0`
+ROOT=$DIR/..
+NODE=${ROOT}/node_modules/.bin/babel-node
+
+for ENV in production development test; do
+  printf "Testing ${ENV}... "
+  RESULT=`NODE_ENV=${ENV} ${NODE} --presets ${ROOT} ${DIR}/test.js`
+  if [ $? -ne 0 ]; then echo "FAIL." && exit 1; fi;
+  if [ "${RESULT}" -ne 3 ]; then echo "FAIL." && exit 1; fi;
+  echo "OK."
+done
